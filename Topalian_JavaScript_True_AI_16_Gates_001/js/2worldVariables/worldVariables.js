d68f18c94bd1dd0c9380e20ec5c95b392f588195
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-03-07 14:50:03 -0500

    Add files via upload

---
+++ Topalian_JavaScript_True_AI_16_Gates_001/js/2worldVariables/worldVariables.js
@@ -0,0 +1,12 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// worldVariables.js
+
+let online = false;
+
