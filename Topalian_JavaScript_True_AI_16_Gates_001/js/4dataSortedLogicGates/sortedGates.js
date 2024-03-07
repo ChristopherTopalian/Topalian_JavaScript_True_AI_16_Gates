87eb718ea499dbe8d164604d7ffb35f7067423e7
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-03-07 14:50:44 -0500

    Add files via upload

---
+++ Topalian_JavaScript_True_AI_16_Gates_001/js/4dataSortedLogicGates/sortedGates.js
@@ -0,0 +1,93 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// sortedArray.js
+
+let sortedArray =
+[
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Tautology';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Converse Implication';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Xnor';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Right Projection';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Nor';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Left Complementation';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Nand';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Material NonImplication';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Contradiction';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Converse NonImplication';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Xor';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Right Complementation';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Or';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Left Projection';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'And';
+    }),
+
+    logicGates.find(function(gate)
+    {
+        return gate.name === 'Material Implication';
+    })
+];
+
